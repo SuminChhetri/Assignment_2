a031422bac6e72224dfa655c8f069612532d7bb9
SuminChhetri <sghartic@my.centennialcollege.ca>
2023-10-18 22:50:48 -0400

    first commit

---
+++ config/config.js
@@ -0,0 +1,9 @@
+const config = {
+    env: process.env.NODE_ENV || 'development', 
+    port: process.env.PORT || 3000,
+     jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key", 
+    mongoUri: process.env.MONGODB_URI || "mongodb+srv://sghartic:sumingc@Marketplace.nxqtcuo.mongodb.net/Marketplace?retryWrites=true&w=majority"
+   
+    };
+    
+    export default config
